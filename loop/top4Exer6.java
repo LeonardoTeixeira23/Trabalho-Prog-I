c204c9d7904f75e9518396ff73afa74d134d3123
LeonardoTeixera23 <Leo.dst23@gmail.com>
2021-12-30 15:30:52 -0300

    2

--- loop/top4Exer6.java
+++ loop/top4Exer6.java
@@ -14,26 +14,32 @@ public class top4Exer6 {
         cresc1 = kboard.nextInt();
         altu2 = kboard.nextInt();
         cresc2 = kboard.nextInt();
+
         //verifica se a arvore menor pode passar a maior
-    if (altu1 < altu2 && cresc1 <= cresc2 ||
+
+        if (altu1 < altu2 && cresc1 <= cresc2 ||
         altu2 < altu1 && cresc2 <= cresc1 ||
         altu1 == altu2 && cresc1 == cresc2) {
         
             System.out.println("nunca");
     
         } else {
+
             //calcula quanto tempo a arvore 1 demorara caso seja a menor
             if (cresc1 > cresc2) {
                 
                 difAltu= altu2 - altu1;
                 anos= difAltu/(cresc1 - cresc2);
+
                 System.out.println(anos+1);
             }
+
             //calcula quanto tempo a arvore 2 demorara caso seja a menor
             if (cresc2 > cresc1) {
                 
                 difAltu = altu1 - altu2;
                 anos= difAltu/(cresc2 - cresc1);
+
                 System.out.println(anos+1);
             }
         }
