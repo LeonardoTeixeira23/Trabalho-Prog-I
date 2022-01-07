7e708c3c9aa5b93c9223049f26bd49c625f2b9c5
LeonardoTeixera23 <Leo.dst23@gmail.com>
2022-01-07 14:39:25 -0300

    s

--- arrays/top5Exer5.java
+++ arrays/top5Exer5.java
@@ -4,22 +4,50 @@ public class top5Exer5{
     public static void main (String[] args){
         Scanner kboard = new Scanner(System.in);
         
-        int fibonacci[] = new int[30], i, inicio, fim, indexI, indexF;
+        int fibonacci[] = new int[31], i, start, end, indexS=0, indexE=0;
         fibonacci[0] = 0;
         fibonacci[1]= 1;
 
-        inicio = kboard.nextInt();
-        fim = kboard.nextInt();
+        start = kboard.nextInt();
+        end = kboard.nextInt();
 
         for(i = 1; i < fibonacci.length-1; i++){
             fibonacci[i+1] = fibonacci[i]+fibonacci[i-1];
+        
         }
 
-        for(i=0; i < fibonacci.length; i++){
-            if (fibonacci[i] >= inicio && fibonacci[i] <= fim) {
-                System.out.print(fibonacci[i] + " ");
-            
+        if (end >= fibonacci[30] || start > end) {
+
+            System.out.println("erro");
+        }else{
+                    
+            for(i=0; i < fibonacci.length; i++){
+                
+                if (fibonacci[i] >= start && fibonacci[i] <= end) {
+                    System.out.print(fibonacci[i] + " ");
+                    indexE = i;
+                }
             }
-        }    
+
+            if (start == 0) {
+                indexS = 0;
+
+            }else{
+                
+                for ( i = indexE;fibonacci[i] > 0; i-=1) {
+                    if (fibonacci[i] >= start) {
+                        indexS = i;
+                    }
+                }
+            }
+
+            if (indexE == 0) {
+                System.out.println("-");
+
+            } else {
+                System.out.println(indexS+"-"+indexE);
+                
+            }
+        }
     }
 }
